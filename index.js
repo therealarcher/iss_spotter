73a5972429e7c1e89de0bee05e1a3dae858fe46f
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-08 03:27:26 +0000

    added to satisfy ISS Spotter II assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss')
+//const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -10,7 +10,11 @@ const { fetchCoordsByIP } = require('./iss')
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP ('172.46.2.206', (error, data) => {
-  console.log(error);
-  console.log(data);
+fetchCoordsByIP ('172.46.2.206', (error, coords) => {
+  if (error) {
+    console.log(`It didn't work! error: `, error);
+    return;
+  }
+  
+  console.log('It worked! Returned Coords:, ', coords);
 });
--- iss.js
+++ iss.js
@@ -32,14 +32,25 @@ const fetchMyIP = function(callback) {
 };
 
 const fetchCoordsByIP = function(ip, callback) {
-//   request('https://api.ipify.org?format=json', (error, response, body) => {
-//     //console.log('error:', error); // Print the error if one occurred
-//     //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//     //console.log('body:', body); // Print the HTML for the Google homepage.
-//     //const ip = JSON.parse(body).ip;
-//     const ipObj = JSON.parse(body);
-//     const ip = ipObj.ip;
-// });
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    // console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `StatusCode ${response.statusCode} when grabbing coordinates for IP.  Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const {latitude, longitude} = JSON.parse(body).data; // es6 syntax
+    // console.log(latitude);
+    
+    callback(null, {latitude, longitude});
+  });
 };
 
 module.exports = {fetchMyIP};
